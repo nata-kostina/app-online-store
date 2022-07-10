86a1f7de0585fd07d9d365c4f061ab38fdfa1dcc
Natallia Kostsina <natakostina123@gmail.com>
2022-07-10 15:49:28 +0200

    refactor: delete unnecessary files and imports

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -1,7 +1,5 @@
 import "./style.scss"
 import AppController from './controllers/AppController';
-import { CallbackVoid } from "./types/types";
-import noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 const app = new AppController();
 //app.start();
