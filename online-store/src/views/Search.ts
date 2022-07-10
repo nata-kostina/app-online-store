9709bfce66e7fee712a842692ead8d558f7b7850
Natallia Kostsina <natakostina123@gmail.com>
2022-07-10 13:05:08 +0200

    feat: implement search products functionality

---
+++ online-store/src/views/Search.ts
@@ -0,0 +1,40 @@
+import { Actions, EventHandler, Handler, IProduct } from "../types/types";
+import ProductView from './ProductView';
+
+class Search {
+  private handler: EventHandler;
+  constructor(handler: EventHandler) {
+    this.handler = handler;
+    
+    const container = document.createElement('div');
+    container.classList.add('search-container');
+
+    const search = document.createElement('input');
+    search.classList.add('input', 'input-search');
+    search.type = 'search';
+    search.placeholder = 'Search a product...';
+    search.maxLength = 30;
+    search.autocomplete = 'false';
+    search.focus();
+    search.addEventListener('input', (e) => {
+      console.log(e);
+      handler(e, Actions.SEARCH);
+    });
+
+    const btnClear = document.createElement('button');
+    btnClear.classList.add('btn', 'btn-clear-search');
+    btnClear.innerHTML = 'Clear';
+    btnClear.addEventListener('click', (e) => this.handler(e, Actions.CLEAR_SEARCH));
+    
+    container.append(search, btnClear);
+    document.querySelector('main')?.insertAdjacentElement('beforeend', container);
+
+  }
+
+  render(): void {
+    return
+  }
+
+}
+
+export default Search;
